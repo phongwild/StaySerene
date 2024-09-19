a203baf14583185992d24928c3028e2b9e2556a3
VanHoang1504 <hoangcvph42448@fpt.edu.vn>
2024-09-20 00:21:57 +0700

    hiiifdfd

--- app/src/main/java/phongtaph31865/poly/stayserene/Homescreen.java
+++ app/src/main/java/phongtaph31865/poly/stayserene/Homescreen.java
@@ -1,6 +1,10 @@
 package phongtaph31865.poly.stayserene;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +19,12 @@ public class Homescreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_homescreen);
-
+        ImageView imghome1 = findViewById(R.id.imghome1);
+        imghome1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              startActivity(new Intent(Homescreen.this, Homescreen2.class));
+            }
+        });
     }
 }
--- app/src/main/java/phongtaph31865/poly/stayserene/Screen_user/Fragment_home.java
+++ app/src/main/java/phongtaph31865/poly/stayserene/Screen_user/Fragment_home.java
@@ -1,5 +1,6 @@
 package phongtaph31865.poly.stayserene.Screen_user;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +8,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import phongtaph31865.poly.stayserene.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment_home#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Fragment_home extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Fragment_home() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_home.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment_home newInstance(String param1, String param2) {
-        Fragment_home fragment = new Fragment_home();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    ImageView imghome1;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+//        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+//        imghome1 = view.findViewById(R.id.imghome1);
+//        imghome1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////
+////                startActivity(new Intent(Fragment_home.this, Fragment_booking.class));
+//            }
+//        });
+        return view;
     }
 }
