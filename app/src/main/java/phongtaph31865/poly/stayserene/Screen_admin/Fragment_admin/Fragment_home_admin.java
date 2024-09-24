70c877773d316516cb51f47316af91d2db23c387
acertuananh <anhttph43396@fpt.edu.vn>
2024-09-24 22:19:44 +0700

    update

--- app/src/main/java/phongtaph31865/poly/stayserene/Screen_admin/Fragment_admin/Fragment_home_admin.java
+++ app/src/main/java/phongtaph31865/poly/stayserene/Screen_admin/Fragment_admin/Fragment_home_admin.java
@@ -3,64 +3,66 @@ package phongtaph31865.poly.stayserene.Screen_admin.Fragment_admin;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import phongtaph31865.poly.stayserene.CardAdapter;
+import phongtaph31865.poly.stayserene.CardModel;
 import phongtaph31865.poly.stayserene.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment_home_admin#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Fragment_home_admin extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Fragment_home_admin() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_home_admin.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment_home_admin newInstance(String param1, String param2) {
-        Fragment_home_admin fragment = new Fragment_home_admin();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    private RecyclerView recyclerView1;
+    private RecyclerView recyclerView2;
+    private CardAdapter cardAdapter;
+    private List<CardModel> cardList1;
+    private List<CardModel> cardList2;
+    //    private FloatingActionButton fab;
+//    private BottomNavigationView bottomNavigationView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_admin, container, false);
+        View view = inflater.inflate(R.layout.fragment_home_admin, container, false);
+
+        // Tìm các RecyclerView và các thành phần khác bằng view
+        recyclerView1 = view.findViewById(R.id.recyclerView1);
+        recyclerView2 = view.findViewById(R.id.recyclerView2);
+//        FloatingActionButton fab = view.findViewById(R.id.fab);
+//        BottomNavigationView bottomNav = view.findViewById(R.id.bottomNavigationView);
+
+        // Cài đặt Layout cho RecyclerView
+        LinearLayoutManager layoutManager1 = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerView1.setLayoutManager(layoutManager1);
+        LinearLayoutManager layoutManager2 = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerView2.setLayoutManager(layoutManager2);
+
+        // Tạo danh sách 20 thẻ cho RecyclerView1
+        cardList1 = new ArrayList<>();
+        for (int i = 1; i <= 20; i++) {
+            cardList1.add(new CardModel("1d3d432dfd" + i, "4dj1d0q1" + i, "18/9/2024", "2,000,000", "Online payment", "Wait for confirmation"));
+        }
+
+        // Tạo danh sách 20 thẻ cho RecyclerView2
+        cardList2 = new ArrayList<>();
+        for (int i = 1; i <= 20; i++) {
+            cardList2.add(new CardModel("1d3d432dfd" + i, "4dj1d0q1" + i, "18/9/2024", "2,000,000", "Online payment", "Confirmed"));
+        }
+
+        // Gán Adapter cho RecyclerView1 và RecyclerView2
+        cardAdapter = new CardAdapter(cardList1, getContext());
+        recyclerView1.setAdapter(cardAdapter);
+        cardAdapter = new CardAdapter(cardList2, getContext());
+        recyclerView2.setAdapter(cardAdapter);
+
+        return view; // Trả về view đã inflate
     }
+
 }
--- app/src/main/java/phongtaph31865/poly/stayserene/Screen_admin/Fragment_admin/Fragment_statical_admin.java
+++ app/src/main/java/phongtaph31865/poly/stayserene/Screen_admin/Fragment_admin/Fragment_statical_admin.java
@@ -7,60 +7,55 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import phongtaph31865.poly.stayserene.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment_statical_admin#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Fragment_statical_admin extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Fragment_statical_admin() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_statical_admin.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment_statical_admin newInstance(String param1, String param2) {
-        Fragment_statical_admin fragment = new Fragment_statical_admin();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statical_admin, container, false);
+        View view = inflater.inflate(R.layout.fragment_statical_admin, container, false);
+
+        // Khởi tạo các view từ file layout bằng cách sử dụng view.findViewById()
+        TextView title = view.findViewById(R.id.title);
+        TextView date = view.findViewById(R.id.date);
+        Button btnThongKe = view.findViewById(R.id.btnThongKe);
+        TableLayout tableLayout = view.findViewById(R.id.tableLayout);
+
+        // Thêm sự kiện cho nút Thống kê
+        btnThongKe.setOnClickListener(v -> {
+            // Xử lý logic thống kê tại đây
+            // Ví dụ: bạn có thể thêm dữ liệu mới vào bảng
+
+            // Thêm một hàng mới vào bảng
+            TableRow tableRow = new TableRow(getContext());
+
+            TextView maPhong = new TextView(getContext());
+            maPhong.setText("P002");
+            tableRow.addView(maPhong);
+
+            TextView tenPhong = new TextView(getContext());
+            tenPhong.setText("Phòng 2");
+            tableRow.addView(tenPhong);
+
+            TextView time = new TextView(getContext());
+            time.setText("10:00 - 11:00");
+            tableRow.addView(time);
+
+            TextView tongTien = new TextView(getContext());
+            tongTien.setText("250.000 VNĐ");
+            tableRow.addView(tongTien);
+
+            tableLayout.addView(tableRow);  // Thêm hàng mới vào bảng
+        });
+
+        return view;  // Trả về view đã inflate
     }
 }
