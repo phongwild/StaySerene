054b82d02fc19992bf20002de1ada552d4557e11
VanHoang1504 <hoangcvph42448@fpt.edu.vn>
2024-10-30 23:42:05 +0700

    update xem chi tiet loai phong

--- app/src/main/java/phongtaph31865/poly/stayserene/Screen_user/Activity/Activity_detail_type_rooms.java
+++ app/src/main/java/phongtaph31865/poly/stayserene/Screen_user/Activity/Activity_detail_type_rooms.java
@@ -2,6 +2,10 @@ package phongtaph31865.poly.stayserene.Screen_user.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +13,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import phongtaph31865.poly.stayserene.Api_service.Api_service;
+import phongtaph31865.poly.stayserene.Model.Account;
+import phongtaph31865.poly.stayserene.Model.Hotel;
+import phongtaph31865.poly.stayserene.Model.TypeRoom;
 import phongtaph31865.poly.stayserene.R;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Activity_detail_type_rooms extends AppCompatActivity {
+    private ImageView btn_back_detail_type_room, img_detail_type_room;
+    private TextView tv_name_detail_type_room, tv_room_name_detail, tv_location_detail_type_room, tv_price_detail_type_room, tv_so_luong_phong_detail_type_room, tv_tienNghi_detail_type_room, tv_dienTich_detail_type_room, tv_description_detail_type_room;
+    private LinearLayout btn_booking_detail_type_room;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +38,58 @@ public class Activity_detail_type_rooms extends AppCompatActivity {
         setContentView(R.layout.activity_detail_type_rooms);
         Intent intent = getIntent();
         String id_type_room = intent.getStringExtra("id_type_room");
+        btn_back_detail_type_room = findViewById(R.id.btn_back_detail_type_room);
+        img_detail_type_room = findViewById(R.id.img_detail_type_room);
+        tv_name_detail_type_room = findViewById(R.id.tv_name_detail_type_room);
+
+        tv_location_detail_type_room = findViewById(R.id.tv_location_detail_type_room);
+        tv_price_detail_type_room = findViewById(R.id.tv_price_detail_type_room);
+        tv_so_luong_phong_detail_type_room = findViewById(R.id.tv_so_luong_phong_detail_type_room);
+        tv_tienNghi_detail_type_room = findViewById(R.id.tv_tienNghi_detail_type_room);
+        tv_dienTich_detail_type_room = findViewById(R.id.tv_dienTich_detail_type_room);
+        tv_description_detail_type_room = findViewById(R.id.tv_description_detail_type_room);
+        if (id_type_room != null) {
+            Api_service.service.get_typeroom().enqueue(new Callback<List<TypeRoom>>() {
+                @Override
+                public void onResponse(Call<List<TypeRoom>> call, Response<List<TypeRoom>> response) {
+                    if (response.isSuccessful()) {
+                        for (TypeRoom typeroom : response.body()) {
+                            if (typeroom.get_id().equals(id_type_room)) {
+                                tv_dienTich_detail_type_room.setText(String.valueOf(typeroom.getDienTich()));
+                                tv_tienNghi_detail_type_room.setText(String.valueOf(typeroom.getTienNghi()));
+                                tv_so_luong_phong_detail_type_room.setText(String.valueOf(typeroom.getSoLuongPhong()));
+                                tv_description_detail_type_room.setText(typeroom.getMoTaLoaiPhong());
+                                tv_price_detail_type_room.setText(String.valueOf(typeroom.getGiaLoaiPhong()));
+                                Picasso.get().load(typeroom.getAnhLoaiPhong()).into(img_detail_type_room);
+                                String idHotel = typeroom.getIdKhachSan();
+                                Api_service.service.get_hotel().enqueue(new Callback<List<Hotel>>() {
+                                    @Override
+                                    public void onResponse(Call<List<Hotel>> call, Response<List<Hotel>> response) {
+                                        if (response.isSuccessful()){
+                                            for(Hotel hotel: response.body()){
+                                                if (hotel.get_id().equals(idHotel)){
+                                                    tv_location_detail_type_room.setText(hotel.getDiaChi());
+                                                }
+                                            }
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onFailure(Call<List<Hotel>> call, Throwable throwable) {
+
+                                    }
+                                });
+                            }
+                        }
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<List<TypeRoom>> call, Throwable throwable) {
+                    Log.e("dsfs", throwable.getMessage());
+                }
+            });
+        }
 
     }
 }
