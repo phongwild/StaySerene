10d6d88bc1aaef2f6f6afc97062913a786c26d42
acertuananh <anhttph43396@fpt.edu.vn>
2024-11-24 21:21:09 +0700

    update layout booking

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,11 @@ dependencies {
         "include" to listOf("*.aar", "*.jar"),
         "exclude" to listOf("")
     )))
+    implementation(fileTree(mapOf(
+        "dir" to "D:\\Totnghiep\\StaySerene\\app\\src\\main\\java\\phongtaph31865\\poly\\stayserene\\SDK_zalo",
+        "include" to listOf("*.aar", "*.jar"),
+        "exclude" to listOf("")
+    )))
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
